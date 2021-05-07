45316164e503d208abedb941cf8543e9def67bff
Bund <wilhelmagren98@gmail.com>
2021-05-07 12:56:11 +0200

    normalization

--- lib/cnn.py
+++ lib/cnn.py
@@ -18,6 +18,8 @@ class CNN:
         self.y_validation = None
         self.x_test = None
         self.y_test = None
+        self.target_mean = 0
+        self.target_std = 0
         self.verbose = verbose
         self.NUM_FEATURES = 7
         self.DEFAULT_MODEL_FILEPATH = '../model/CNN_weights'
@@ -62,6 +64,14 @@ class CNN:
     def model_summary(self):
         self.model.summary()
 
+    def normalize_labels(self, labels):
+        labels[labels > 80] = 80
+        labels[labels < -80] = -80
+        self.target_mean = np.mean(labels)
+        self.target_std = np.std(labels)
+        labels = (labels - np.mean(labels))/np.std(labels)
+        return labels
+
     def read_files(self):
         data = []
         column_list = []
@@ -84,6 +94,7 @@ class CNN:
         x_data, y_data = self.read_files()
         x_train = np.concatenate(x_data, axis=0)
         y_train = np.concatenate(y_data, axis=0)
+        y_train = self.normalize_labels(y_train)
         x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.2)
         x_valid, x_test, y_valid, y_test = train_test_split(x_valid, y_valid, test_size=0.5)
         self.x_train = x_train.reshape(len(x_train), 8, 8, 7)
@@ -106,7 +117,7 @@ class CNN:
             if loss is None:
                 print(f'<|\t\tNo loss specified\t\t\t=>  using default: {self.loss}')
             loss = self.loss
-        callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=4)
+        callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
         self.model.compile(optimizer=optimizer, loss=loss)
         self.history = self.model.fit(self.x_train, self.y_train, epochs=n_epochs, validation_data=(self.x_validation, self.y_validation), callbacks=[callback])
 
@@ -118,6 +129,12 @@ class CNN:
         plt.legend()
         plt.show()
 
+    def plot_histogram(self):
+        plt.hist(self.y_test, bins=500)
+        plt.xlabel('evaluation')
+        plt.ylabel('num labels')
+        plt.show()
+
     def model_predict(self, offset=0.5):
         y = self.model.predict(self.x_test)
 
@@ -127,13 +144,19 @@ class CNN:
                   f'\n\t\tlen(prediction)={len(y)} :: len(target)={len(self.y_test)}')
         acc = 0
         diff = []
+        diff_vanilla = []
         for (target, predicted) in zip(self.y_test, y):
-            print(f'target={target} :: predicted={predicted}')
-            if target - offset <= predicted <= target + offset:
-                acc += 1
+            #print(f'target={target} :: predicted={predicted}')
+            #if target - offset <= predicted <= target + offset:
+            #    acc += 1
+            target_vanilla = (target + self.target_mean)*self.target_std
+            predicted_vanilla = (predicted + self.target_mean)*self.target_std
             diff.append(np.abs(target - predicted))
-        print(f'<|\tModel testing accuracy:\t {100*round(float(acc)/float(len(y)), 4)}%')
-        print(f'<|\tModel mean error:\t\t {np.mean(np.array(diff))}')
+            diff_vanilla.append(np.abs(target_vanilla - predicted_vanilla))
+            print(f'\t{predicted}  =>  {predicted_vanilla}  vs  {target_vanilla}')
+        # print(f'<|\tModel testing accuracy:\t {100*round(float(acc)/float(len(y)), 4)}%')
+        print(f'<|\tModel mean error normalized:\t\t {np.mean(np.array(diff))}')
+        print(f'<|\tModel mean error:\t\t\t {np.mean(np.array(diff_vanilla))}')
 
 
 def main():
@@ -143,10 +166,13 @@ def main():
     model.init_model()
     # model.load_model()
     model.parse_data()
-    model.batch_train(n_epochs=60)
+    # model.plot_histogram()
+    #"""
+    model.batch_train(n_epochs=40)
     model.save_model()
     model.plot_history()
     model.model_predict()
+    #"""
 
 
 if __name__ == '__main__':
